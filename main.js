cbda13be84bdcfde4ab4bcf0dc603c13046e7153
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-02-06 23:23:52 +1100

    created for loop in window.onload async function to loop through pokemon

--- main.js
+++ main.js
@@ -3,19 +3,30 @@ var pokedex = {}; // {1 : {"name" : "bulbasaur", "img" : url, "type" : ["grass",
 
 // async function because using await
 window.onload = async function() {
-    getPokemon(1);
+    // getPokemon(1);
+    for (let i = 1; i <= pokemonCount; i++) {
+        await getPokemon(i);
+    }
+    console.log(pokedex);
 }
 
 // async function because using await
 async function getPokemon(num) {
     let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
 
+    let res = await fetch(url);
+    let pokemon = await res.json();
+    // console.log(pokemon)
 
-let res = await fetch(url);
-let pokemon = await res.json();
-// console.log(pokemon)
+    let pokemonName = pokemon["name"]
+    let pokemonType = pokemon["types"]
+    let pokemonImg = pokemon["sprites"]["front_default"];
 
-let pokemonName = pokemon["name"]
-let pokemonTypes = pokemon["types"]
-let pokemonImg = pokemon["sprites"]["front_default"];
+    res = await fetch(pokemon["species"]["url"]);
+    let pokemonDesc = await res.json();
+
+    // console.log(pokemonDesc);
+    pokemonDesc = pokemonDesc["flavor_text_entries"][0]["flavor_txt"];
+
+    pokedex[num] = {"name" : pokemonName, "img" : pokemonImg, "types" : pokemonType, "desc" : pokemonDesc}
 }
