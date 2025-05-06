60d52afd88a68b6188238bbf73b9ef196b9a4e80
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2025-05-06 15:14:05 +1000

    proper heart emoji

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", initialize);
 
 const EMPTY_HEART = '♡';
-const FULL_HEART = '♥';
+const FULL_HEART = '❤️';
 const pokemonCount = 151;
 var pokedex = {};
 var likedPokemon = [];
